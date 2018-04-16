457cb25957d3b89bc3b433975e1dbd2cf5f5f646
bjxmaru <598bieyifeng@163.com>
2018-04-16 10:56:33 +0800

    revise bootstrap , juery , bootstrap-datetimepicker for  seajs

---
+++ bimanage/bggweb/src/main/webapp/th/module/js/index-01.js
@@ -0,0 +1,43 @@
+define(function (require, exports ,module) {
+    var $ = require("jquery") ;
+    require("bootstrap");
+
+    require("bootstrap-datetimepicker");
+
+    console.log($.prototype);
+    console.log($(".form-date"));
+
+       $(".form-date").datetimepicker({
+           //language: 'zh-CN',//显示中文
+           format: 'yyyy-mm-dd',//显示格式
+           minView: "month",//设置只显示到月份
+           initialDate: new Date(),//初始化当前日期
+           autoclose: true,//选中自动关闭
+           todayBtn: true//显示今日按钮
+       }) ;
+
+    //var a = require("../static/hello/src/index/index-01-aux") ;
+    //var a = require("js/index-01-aux").a ;
+
+    //alert( a) ;
+
+
+    var urlStr1 = "http://192.168.50.170:8080/bggweb/jasper/corpInfo.action" ;
+    var urlStr2 = "http://www.sina.com.cn" ;
+
+  /*  $("#fun1").click(function () {
+
+       //alert( $("#main_frame").contentDocument.getElementsByTagName("input")[0].value );
+        alert($("#main_frame").contents().find("input[name='paraYear']").val());
+
+        // $("#main_frmame").attr("src",urlStr1 );
+
+
+    });
+
+    $("#fun2").click(function () {
+
+        $("#main_frmame").attr("src", urlStr2 );
+    });
+*/
+}) ;
