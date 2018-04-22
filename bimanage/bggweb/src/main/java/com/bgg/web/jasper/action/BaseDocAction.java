d006103a49da1e2342674fbd5bfe22985efb1193
bjxmaru <598bieyifeng@163.com>
2018-04-22 18:22:25 +0800

    begin add the jasper desing file

--- bimanage/bggweb/src/main/java/com/bgg/web/jasper/action/BaseDocAction.java
+++ bimanage/bggweb/src/main/java/com/bgg/web/jasper/action/BaseDocAction.java
@@ -1,17 +1,48 @@
 package com.bgg.web.jasper.action;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.ValueStack;
+import com.sun.tools.classfile.StackMapTable_attribute;
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+import org.apache.struts2.ServletActionContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller("BaseDocAction")
 @Scope("prototype")
 public class BaseDocAction extends ActionSupport{
 
+    String bgg = "bgg";
+
+    String servletActionContext_reuqest =  ServletActionContext.HTTP_REQUEST;
 
     public String corpInfo() {
 
         return "corpInfo";
+    }
+
+    public String test() {
+
+
+        ValueStack valueStack = ActionContext.getContext().getValueStack();
+
+        HttpServletRequest  httpServletRequest =(HttpServletRequest)  ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
+
+        boolean  a = true;
+
+        boolean b = true ;
+        boolean c = true ;
+
+        boolean d =  (!a &&  b );
+
+        System.out.println(b);
+
+
+
+        return "test" ;
     }
 }
---
+++ bimanage/bggweb/src/main/webapp/WEB-INF/jsp/test.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bjx
+  Date: 2018/4/20
+  Time: 18:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib  uri="/struts-tags" prefix="s"   %>
+<html>
+<head>
+    <title>jsp test jasper</title>
+    <style>
+        .testContainer {
+            width:  900px;
+            height: 900px;
+            border: 1px solid #000;
+        }
+    </style>
+</head>
+<body>
+
+   <div class="testContainer">
+
+        <s:debug></s:debug>
+   </div>
+</body>
+</html>
